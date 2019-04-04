0c4a65099078cbb9904c2644aef9ab18aa6ef6b0
Yungprince312 <aamerlayne@gmail.com>
2019-04-04 14:02:57 -0400

    added star rating

--- client/main.js
+++ client/main.js
@@ -5,20 +5,78 @@ import './main.html';
 import '../lib/collection.js';
 
 Template.myJumbo.events({
-	'click .js-adding'(event){
-		$("#addimgmodal").modal("show");
+	'click .js-addImg'(event){
+		$("#addImgModal").modal("show");
 	}
-
 });
 
-Template.Adding.events({
+Template.addImg.events({
 	'click .js-saveImg'(event){
+		var imgTitle = $("#imgTitle").val();
 		var imgPath = $("#imgPath").val();
-		var TitlePath = $("#TitlePath").val();
-		var DescPath = $("#DescPath").val();
-		console.log("save", imgPath, TitlePath, DescPath);
-		$("#addimgmodal").modal("hide");
-		ImagesDB.insert({'firstname':fName,
-  		'lastname':lName, 'img':Photopic});
+		var imgDesc = $("#imgDesc").val();
+		
+		$("#imgTitle").val('');
+		$("#imgPath").val('');
+		$("#imgDesc").val('');
+		$("#addImgPreview").attr('src','user-512.png');
+		$("#addImgModal").modal("hide");
+		ImagesDB.insert({"title":imgTitle, "path":imgPath, "desc":imgDesc, "createdOn":Date()});
+	},
+	'click .js-cancelAdd'(){
+		$("#imgTitle").val('');
+		$("#imgPath").val('');
+		$("#imgDesc").val('');
+		$("#addImgPreview").attr('src','user-512.png');
+		$("#addImgModal").modal("hide");
+	},
+	'input #imgPath'(event){
+		var imgPath = $("#imgPath").val();
+		$("#addImgPreview").attr('src', imgPath);
+	}
+});
+
+Template.mainBody.helpers({
+	imagesFound(){
+		return ImagesDB.find().count();
+	},
+	allImages(){
+		return ImagesDB.find({}, {sort:{imgRate: -1}});
+	}
+});
+
+Template.mainBody.events({
+	'click .js-deleteImg'(){
+		var imgId = this._id;
+		$("#"+imgId).fadeOut('slow', function(){
+			ImagesDB.remove({_id:imgId});
+		});
+	},
+	'click .js-editImage'(){
+		var imgId = this._id;
+		$('#ImgPreview').attr('src',ImagesDB.findOne({_id:imgId}).path);
+		$("#eimgTitle").val(ImagesDB.findOne({_id:imgId}).title);
+		$("#eimgPath").val(ImagesDB.findOne({_id:imgId}).path);
+		$("#eimgDesc").val(ImagesDB.findOne({_id:imgId}).desc);
+		$('#eId').val(ImagesDB.findOne({_id:imgId})._id);
+		$('#editImgModal').modal("show");
+	},
+	'click .js-rate'(event){
+		var imgId = this.data_id;
+		var rating = $(event.currentTarget).data('userrating');
+		ImagesDB.update({_id:imgId}, {$set:{'imgRate':rating}})
+		//console.log("you clicked a star",imgId "with a rating of", rating);
+		
+	}
+});
+
+Template.editImg.events({
+	'click .js-updateImg'(){
+		var eId = $('#eId').val();
+		var imgTitle = $("#eimgTitle").val();
+		var imgPath = $("#eimgPath").val();
+		var imgDesc = $("#eimgDesc").val();
+		ImagesDB.update({_id:eId}, {$set:{"title":imgTitle, "path":imgPath, "desc":imgDesc}});
+		$('#editImgModal').modal("hide");
 	}
 });
